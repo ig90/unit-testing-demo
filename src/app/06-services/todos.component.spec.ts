18bca88c071e00622e4b897d3186dfbea2ba0bdb
Igor <igor.filipiuk@gmail.com>
2019-03-27 21:07:42 +0100

    added tests for add method

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,6 +1,6 @@
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
-import { from } from '../../../node_modules/rxjs';
+import { from, empty, throwError } from '../../../node_modules/rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -17,5 +17,26 @@ describe('TodosComponent', () => {
     });
     component.ngOnInit();
     expect(component.todos).toBe(todos);
+  });
+  it('should call the server to save the changes when a new todo item is added', () => {
+    let spy = spyOn(service, 'add').and.callFake(t => {
+      return empty();
+    });
+    component.add();
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should add the new todo returned from the server', () => {
+    let todo = { id: 1 };
+    let spy = spyOn(service, 'add').and.returnValue(from([todo]));
+
+    component.add();
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+  it('should set the message property if server returns an error when adding a new todo', () => {
+    let error = 'error from the server';
+    let spy = spyOn(service, 'add').and.returnValue(throwError(error));
+
+    component.add();
+    expect(component.message).toBe(error);
   });
 });
