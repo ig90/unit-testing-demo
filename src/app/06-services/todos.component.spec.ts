02fe3e9c725cf15b8813d696d6ee99796ed2e98c
Igor <igor.filipiuk@gmail.com>
2019-03-27 19:29:47 +0100

    testing the ngOnInit method using spyOn()

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,21 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { from } from '../../../node_modules/rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
-
+  let service: TodoService;
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
-
-  it('', () => {
+  // testing ngOnInit method
+  it('should set todos property with items returned from the server', () => {
+    let todos = [1, 2, 3];
+    spyOn(service, 'getTodos').and.callFake(() => {  // jasmine function spyOn()
+      return from([ todos ]);
+    });
+    component.ngOnInit();
+    expect(component.todos).toBe(todos);
   });
 });
